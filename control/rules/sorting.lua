16893c528d64083eb66e016bc683876a72b1b229
Adrien 'Larandar' DUDOUIT-EXPOSITO <adrien@dudouit.me>
2020-10-12 19:13:40 +0200

    ⬆️ (Planning): Migrate "by-cheapness" to the rule system

--- control/rules/sorting.lua
+++ control/rules/sorting.lua
@@ -36,4 +36,48 @@ function DepthSorting:apply(force, technologies)
     return {}, technologies
 end
 
-return {["sorting.by-research-depth"] = DepthSorting}
+local CheapnessSorting = {
+    -- By default it's the total raw time is minutes that is used
+    pack_weights = {
+        ["automation-science-pack"] = 0.25,
+        ["logistic-science-pack"] = 0.5,
+        ["military-science-pack"] = 2,
+        ["chemical-science-pack"] = 4,
+        ["utility-science-pack"] = 20,
+        ["production-science-pack"] = 21,
+        ["space-science-pack"] = 100
+    }
+}
+CheapnessSorting.__index = CheapnessSorting
+
+function CheapnessSorting:new(o)
+    o = o or {}
+    setmetatable(o, self)
+    return o
+end
+
+function CheapnessSorting:apply(force, technologies)
+    table.sort(technologies,
+               function(a, b) return self:cost_of(a) < self:cost_of(b) end)
+    return {}, technologies
+end
+
+function CheapnessSorting:cost_of(tech)
+    local pack_cost = 0
+    for _, i in ipairs(tech.research_unit_ingredients) do
+        if self.pack_weights[i.name] then
+            pack_cost = pack_cost + self.pack_weights[i.name]
+        else
+            -- This is a warning
+            print("Could not find pack weight (using default: 10):" .. i.name)
+            print("You can open an github issue to provide a better value.")
+            pack_cost = pack_cost + 10
+        end
+    end
+    return pack_cost * tech.research_unit_count
+end
+
+return {
+    ["sorting.by-research-depth"] = DepthSorting,
+    ["sorting.by-cheapness"] = CheapnessSorting
+}
