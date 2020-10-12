1e1f2659dc236af001f9efd246fa16f9f08b86cd
Adrien 'Larandar' DUDOUIT-EXPOSITO <adrien@dudouit.me>
2020-10-13 00:23:27 +0200

    🔥 (Settings): Removing WIP setting since there is a CLI now

--- settings.lua
+++ settings.lua
@@ -11,11 +11,5 @@ data:extend({
         setting_type = "runtime-global",
         default_value = "secret-to-enemies",
         allowed_values = {"no", "friends", "always", "secret-to-enemies"}
-    }, {
-        type = "string-setting",
-        name = "research-planner-wip-planning-strategy",
-        setting_type = "runtime-global",
-        default_value = "no",
-        allowed_values = {"no", "first-found", "cheapest"}
     }
 })
