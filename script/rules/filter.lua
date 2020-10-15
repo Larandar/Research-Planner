7c1f3aae48fa5bb400bd7039f3f7bcf67af39f40
Adrien 'Larandar' DUDOUIT-EXPOSITO <adrien@dudouit.me>
2020-10-15 17:53:44 +0200

    ♻️ (Filter): Switch filters to use flib

--- script/rules/filter.lua
+++ script/rules/filter.lua
@@ -1,6 +1,5 @@
--- Standard Lib for factorio: https://afforess.github.io/Factorio-Stdlib/
-local table = require("__stdlib__/stdlib/utils/table")
-local string_array = require('__stdlib__/stdlib/utils/classes/string_array')
+-- Factorio-Lib: https://factoriolib.github.io/flib/index.html
+local table = require("__flib__.table")
 
 -- Ban research pack fron the planning
 local BanResearchPacks = {packs = {}}
@@ -8,17 +7,35 @@ BanResearchPacks.__index = BanResearchPacks
 function BanResearchPacks:hydrate(o) setmetatable(o, self) end
 
 function BanResearchPacks:apply(force, technologies)
-    -- Get all banned science-packs
-    local banned_packs = string_array(table.values(self.packs))
-    table.each(self.packs,
-               function(p) banned_packs:add(p .. "-science-pack") end)
+    return {}, table.filter(technologies, self:filter())
+end
 
-    return {}, table.filter(technologies, function(t)
-        local used_packs = table.map(t.research_unit_ingredients,
+function BanResearchPacks:filter()
+    local function contain_banned_sciend(packs, suffix)
+        for _, pack in ipairs(packs) do
+            for _, p in ipairs(self.packs) do
+                if pack == p .. (suffix or "") then return true end
+            end
+        end
+        return false
+    end
+
+    return function(tech)
+        -- Present packs
+        local used_packs = table.map(tech.research_unit_ingredients,
                                      function(i) return i.name end)
-        return not table.any(used_packs,
-                             function(p) return banned_packs:has(p) end)
-    end)
+
+        -- Get all banned science-packs with complete name
+        if contain_banned_sciend(used_packs) then return false end
+
+        -- Get all banned science-packs ending with "-science-packs"
+        if contain_banned_sciend(used_packs, "-science-pack") then
+            return false
+        end
+
+        -- There was no match then
+        return true
+    end
 end
 
 -- Ban research family from the planning
@@ -27,10 +44,22 @@ BanResearchTypes.__index = BanResearchTypes
 function BanResearchTypes:hydrate(o) setmetatable(o, self) end
 
 function BanResearchTypes:apply(force, technologies)
-    return {}, table.filter(technologies, function(t)
-        return not table.any(self.types,
-                             function(r) return t.name:sub(0, #r) == r end)
-    end)
+    return {}, table.filter(technologies, self:filter())
+end
+
+function BanResearchTypes:filter()
+    -- Filtering by prefix
+    local function starts_with(s, p) return s:sub(0, #p) == p end
+
+    return function(tech)
+        -- The first match stop the loockup
+        for _, banned_type in ipairs(self.types) do
+            if starts_with(tech.name, banned_type) then return false end
+        end
+
+        -- There was no match then
+        return true
+    end
 end
 
 return {
